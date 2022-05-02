f7630cb58c6491b305f6977ae987b0aa5dc48e27
joaopjk <joaopjk48@gmail.com>
2022-05-02 00:52:09 -0300

    ContextFactory

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<ContextApi>
+    {
+        public ContextApi CreateDbContext(string[] args)
+        {
+            //Usado para criar migrations em tempo de projeto
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            return new ContextApi(
+                new DbContextOptionsBuilder<ContextApi>()
+                .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)).Options);
+        }
+    }
+}
