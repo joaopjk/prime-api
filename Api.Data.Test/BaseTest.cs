aee9a14e001ea212bafc87ec55f8cd89e586d9eb
joaopjk <joaopjk48@gmail.com>
2022-05-08 00:16:03 -0300

    Criando classe base para testes

---
+++ Api.Data.Test/BaseTest.cs
@@ -0,0 +1,42 @@
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+    }
+
+    public class DbTest : IDisposable
+    {
+        private string _dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public ServiceProvider ServiceProvider { get; set; }
+
+        public DbTest()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<ContextApi>(x =>
+            {
+                x.UseSqlServer($"Server=127.0.0.1,1433;Database={_dataBaseName};user id=SA;Password=Root@123root");
+            }, ServiceLifetime.Transient);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            using var context = ServiceProvider.GetService<ContextApi>();
+            context.Database.EnsureCreated();
+
+        }
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<ContextApi>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
