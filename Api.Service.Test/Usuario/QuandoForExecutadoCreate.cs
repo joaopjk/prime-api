1d775895c089fb65642b711cdaeab45c81163344
joaopjk <joaopjk48@gmail.com>
2022-05-08 16:36:19 -0300

    QuandoForExecutadoUpdate

---
+++ Api.Service.Test/Usuario/QuandoForExecutadoCreate.cs
@@ -0,0 +1,25 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.Usuario
+{
+    public class QuandoForExecutadoCreate : UsuariosTests
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "É possível executar o método Create")]
+        public async Task E_possivel_executar_metodo_create()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(x => x.Post(userDtoCreate)).ReturnsAsync(userDtoCreateResult);
+            _service = _serviceMock.Object;
+            var result = await _service.Post(userDtoCreate);
+            Assert.NotNull(result);
+            Assert.Equal(NomeUsuario, result.Name);
+            Assert.Equal(EmailUsuario, result.Email);
+        }
+    }
+}
---
+++ Api.Service.Test/Usuario/QuandoForExecutadoUpdate.cs
@@ -0,0 +1,33 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.Usuario
+{
+    public class QuandoForExecutadoUpdate : UsuariosTests
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "É possível executar o método Update")]
+        public async Task E_possivel_executar_metodo_create()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(x => x.Post(userDtoCreate)).ReturnsAsync(userDtoCreateResult);
+            _service = _serviceMock.Object;
+            var result = await _service.Post(userDtoCreate);
+            Assert.NotNull(result);
+            Assert.Equal(NomeUsuario, result.Name);
+            Assert.Equal(EmailUsuario, result.Email);
+
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(x => x.Put(userDtoUpdate)).ReturnsAsync(userDtoUpdateResult);
+            _service = _serviceMock.Object;
+            var resultUpdate = await _service.Put(userDtoUpdate);
+            Assert.NotNull(resultUpdate);
+            Assert.Equal(NomeUsuarioAlterado, resultUpdate.Name);
+            Assert.Equal(EmailUsuarioAlterado, resultUpdate.Email);
+        }
+    }
+}
