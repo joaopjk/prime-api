fed6e2eb5fcfec8cce4e3c3bf22d44812da750b0
joaopjk <joaopjk48@gmail.com>
2022-05-08 16:41:29 -0300

    QuandoForExecutadoDelete

---
+++ Api.Service.Test/Usuario/QuandoForExecutadoDelete.cs
@@ -0,0 +1,33 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.Usuario
+{
+    public class QuandoForExecutadoDelete: UsuariosTests
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "É possível executar o método Delete")]
+        public async Task E_possivel_executar_metodo_create()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(x => x.Delete(It.IsAny<Guid>())).ReturnsAsync(true);
+            _service = _serviceMock.Object;
+            var result = await _service.Delete(It.IsAny<Guid>());
+            Assert.True(result);
+
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(x => x.Delete(It.IsAny<Guid>())).ReturnsAsync(false);
+            _service = _serviceMock.Object;
+            result = await _service.Delete(It.IsAny<Guid>());
+            Assert.False(result);
+        }
+    }
+}
