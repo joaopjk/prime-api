296d407afb9e6db75531794070f7110e758eeacc
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2022-05-09 20:17:21 -0300

    ControllerDelete Test

--- Web.Api.Test/Usuario/QuandoRequisitarDelete/Retorno_BadRequest.cs
+++ Web.Api.Test/Usuario/QuandoRequisitarDelete/Retorno_BadRequest.cs
@@ -8,23 +8,23 @@ using Xunit;
 
 namespace Web.Api.Test.Usuario.QuandoRequisitarDelete
 {
-    public class Retorno_Delete
+    public class Retorno_BadRequest
     {
         private UsersController _controller;
 
-        [Fact(DisplayName = "É possível realizar o Delete.")]
-        public async Task E_possivel_invocar_a_controller_delete()
+        [Fact(DisplayName = "Não é possível realizar o Delete.")]
+        public async Task Nao_e_possivel_invocar_a_controller_delete()
         {
             var serviceMock = new Mock<IUserService>();
             serviceMock.Setup(x => x.Delete(It.IsAny<Guid>())).ReturnsAsync(true);
             _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("", "");
 
             var result = await _controller.Delete(It.IsAny<Guid>());
-            Assert.True(result is OkObjectResult);
+            Assert.True(result is BadRequestObjectResult);
 
-            var resultValue = ((OkObjectResult)result).Value;
+            var resultValue = ((BadRequestObjectResult)result).Value;
             Assert.NotNull(resultValue);
-            Assert.True((Boolean)resultValue);
         }
     }
 }
