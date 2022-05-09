110da27eb21defae869783d2f7808831171b9d5d
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2022-05-09 13:47:34 -0300

    Checkpoint

---
+++ Web.Api.Test/Usuario/QuandoRequisitarDelete/Retorno_Delete.cs
@@ -0,0 +1,23 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Moq;
+using prime_api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Web.Api.Test.Usuario.QuandoRequisitarDelete
+{
+    public class Retorno_Delete
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "Não é possível realizar o Delete.")]
+        public async Task Nao_e_possivel_invocar_a_controller_delete()
+        {
+            var serviceMock = new Mock<IUserService>();
+        }
+    }
+}
