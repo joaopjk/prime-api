7874690bec9c59ade22c55c458a5cb9fc0ff48f7
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2022-05-12 14:01:30 -0300

    Checkpoint

---
+++ Api.Integration.Test/Usuario/QuandoRequisitarUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Integration.Test.Usuario
+{
+    public class QuandoRequisitarUsuario : BaseIntegration
+    {
+        private string _name { get; set; }
+        private string _email { get; set; }
+        [Fact]
+        public async Task E_possivel_realizar_CRUD_usuario()
+        {
+
+        }
+    }
+}
